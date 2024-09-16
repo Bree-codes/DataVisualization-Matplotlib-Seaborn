5a08151413571fba35729312014f83f29acfa586
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-09-16 06:50:07 +0300

    Exploring Data Visualization with Matplotlib

--- DataVisualization/DataViz.py
+++ DataVisualization/DataViz.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 x = [1, 2, 3, 4, 5]
 y = [34, 45, 56, 12, 10]
 
+plt.xlabel('Day')
+plt.ylabel('Temp')
+plt.title('Weather Analysis')
 plt.plot(x, y,color='red',linewidth=3)
 plt.show()
 
